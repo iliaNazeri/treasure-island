a2978221bd3603b6c690388bb135d8fff3b2ab96
iliaNazeri <123732273+iliaNazeri@users.noreply.github.com>
2023-01-29 12:00:54 +0330

    Add files via upload

---
+++ Day-01-100.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("welcome to the Band Name Generator! ")
+city = input("what city did you grow up in? ").title()
+pet = input("what is the name of your first pet? ").title()
+Name = city + " " + pet
+print(f"your band name is {Name} ")
+
+
+# In[ ]:
+
+
+
+
